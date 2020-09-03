d3a6556f9781c4bc8d374f1765f8eda77c803190
Asmaathabet <asmathabet2009@hotmail.com>
2020-09-03 19:05:05 +0300

    add basic structure for the app

---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Authenitcate extends StatefulWidget {
+  @override
+  _AuthenitcateState createState() => _AuthenitcateState();
+}
+
+class _AuthenitcateState extends State<Authenitcate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('authenticate'),
+    );
+  }
+}
