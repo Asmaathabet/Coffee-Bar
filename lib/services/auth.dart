64137d55e92f6695fc450a10bd15c640cd4b753c
Asmaathabet <asmathabet2009@hotmail.com>
2020-09-04 11:12:17 +0300

    create model for user called UserData

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,8 +1,14 @@
+import 'package:coffee_bar/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   // (_) means that this property has a private meaning and we can use it only in this file
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user Object based in Firebase user
+  UserData _userFromFirebaseUser(User user) {
+    return user != null ? UserData(uid: user.uid) : null;
+  }
 
   //sign in [Anonymous]
   Future signInAnon() async {
@@ -12,7 +18,7 @@ class AuthService {
       // ignore: deprecated_member_use // instead of FirebaseUser => choose User
       User user = result.user;
 
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
